49edfaeaeed5923c01bd604ce5c1b2290e324b60
Stepan <ifomstep@icloud.com>
2023-03-23 03:44:29 +0500

    Initial Commit

---
+++ WalletTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WalletTracker
+//
+//  Created by Степан Фоминцев on 23.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
