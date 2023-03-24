017ed218c7cbe79c4beac9bb86f9b6104faaf480
Stepan <ifomstep@icloud.com>
2023-03-24 17:56:24 +0500

    Fix

--- WalletTracker/WalletsListTVC + AlertController.swift
+++ WalletTracker/WalletsListTVC + AlertController.swift
@@ -9,17 +9,22 @@ import Foundation
 import UIKit
 
 extension WalletsListTableViewController {
-    func presentAddAlertController(title: String?, message: String?, style: UIAlertController.Style, completion: @escaping (String) -> Void) {
+    func presentAddAlertController(title: String?, message: String?, style: UIAlertController.Style, completion: @escaping (String, String) -> Void) {
         let ac = UIAlertController(title: title, message: message, preferredStyle: style)
         
         ac.addTextField { tf in
             tf.placeholder = "Your SOL address"
         }
+        ac.addTextField { tf2 in
+            tf2.placeholder = "Wallet name"
+        }
         
         let add = UIAlertAction(title: "Add", style: .default) { action in
             guard let walletAddress = ac.textFields?.first?.text else { return }
-            if walletAddress != "" {
-                completion(walletAddress)
+            if let walletName = ac.textFields?.last?.text {
+                if walletAddress != "" {
+                    completion(walletAddress, walletName)
+                }
             }
         }
         let cancel = UIAlertAction(title: "Cancel", style: .destructive)
--- WalletTracker/WalletsListTableViewController.swift
+++ WalletTracker/WalletsListTableViewController.swift
@@ -9,42 +9,68 @@ import UIKit
 
 class WalletsListTableViewController: UITableViewController {
     
+    // MARK: - Properties
     var wallets: [Result] = []
-
+    
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        wallets.append(Result(balance: 12))
+        tableView.separatorStyle = .none
+        self.navigationItem.leftBarButtonItem = editButtonItem
     }
-
+    
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
         1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         wallets.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "walletCell", for: indexPath) as! WalletTableViewCell
         
-//        cell.walletAddressLabel.text = wallets[indexPath.row].address
-        cell.walletBalanceLabel.text = String(wallets[indexPath.row].balance)
+        let currentWallet = wallets[indexPath.row]
         
-
+        cell.cellContentView.layer.cornerRadius = 15
+        cell.walletAddressLabel.text = currentWallet.walletAddress
+        cell.walletNameLabel.text = currentWallet.walletName
+        cell.numberOfNFTsLabel.text = String(currentWallet.num_nfts)
+        cell.numberOfSOLLabel.text = String(format: "%.2f", currentWallet.sol_balance)
+        cell.numberOfTokensLabel.text = String(currentWallet.num_tokens)
+        
         return cell
     }
     
-    @IBAction func addButtonPressed() {
-        presentAddAlertController(title: "Enter SOL address", message: nil, style: .alert) { walletAddress in
-            self.fetchWalletData(address: walletAddress)
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        true
+    }
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            wallets.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+//            tableView.reloadData()
         }
     }
     
     
-    private func fetchWalletData(address: String) {
-        guard let url = URL(string: "https://api.shyft.to/sol/v1/wallet/balance?network=mainnet-beta&wallet=\(address)") else { return }
+    // MARK: - IBActions
+    @IBAction func addButtonPressed() {
+        presentAddAlertController(title: "Enter SOL address", message: nil, style: .alert) { walletAddress, walletName in
+            self.fetchWalletData(address: walletAddress, name: walletName)
+        }
+    }
+    
+    
+    // MARK: - Private Methods
+    private func fetchWalletData(address: String, name: String) {
+        guard let url = URL(string: "https://api.shyft.to/sol/v1/wallet/get_portfolio?network=mainnet-beta&wallet=\(address)") else { return }
         var request = URLRequest(url: url)
         request.addValue("-3iYNcRok7Gm4EMl", forHTTPHeaderField: "x-api-key")
         request.httpMethod = "GET"
@@ -53,7 +79,11 @@ class WalletsListTableViewController: UITableViewController {
             guard error == nil else { return }
             guard let walletData = try? JSONDecoder().decode(Wallet.self, from: data) else { return }
             DispatchQueue.main.async {
-                self.wallets.append(Result(balance: walletData.result.balance))
+                if name != "" {
+                    self.wallets.append(Result(walletName: name, walletAddress: address, sol_balance: walletData.result.sol_balance, num_tokens: walletData.result.num_tokens, num_nfts: walletData.result.num_nfts))
+                } else {
+                    self.wallets.append(Result(walletName: "Wallet \(self.wallets.count + 1)", walletAddress: address, sol_balance: walletData.result.sol_balance, num_tokens: walletData.result.num_tokens, num_nfts: walletData.result.num_nfts))
+                }
                 self.tableView.reloadData()
                 print(self.wallets)
             }
