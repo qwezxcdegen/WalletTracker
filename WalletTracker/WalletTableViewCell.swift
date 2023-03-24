3e3b09bc040743d0ba2089d9534cc27d45dfccc3
Stepan <ifomstep@icloud.com>
2023-03-24 17:56:13 +0500

    Cell modify

--- WalletTracker/WalletTableViewCell.swift
+++ WalletTracker/WalletTableViewCell.swift
@@ -9,8 +9,16 @@ import UIKit
 
 class WalletTableViewCell: UITableViewCell {
     
+    @IBOutlet weak var cellContentView: UIView!
+    
+    @IBOutlet weak var walletNameLabel: UILabel!
+    
     @IBOutlet weak var walletAddressLabel: UILabel!
     
-    @IBOutlet weak var walletBalanceLabel: UILabel!
+    @IBOutlet weak var numberOfNFTsLabel: UILabel!
+    
+    @IBOutlet weak var numberOfSOLLabel: UILabel!
+    
+    @IBOutlet weak var numberOfTokensLabel: UILabel!
     
 }
